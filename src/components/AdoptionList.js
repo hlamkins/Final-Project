326dec6bfb1509d3c977e53697a5f0e257dd09c7
Randy Whitley <randywhitley84@gmail.com>
2021-03-09 20:14:10 -0600

    created save and home page

---
+++ src/components/AdoptionList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const AdoptionList = (props) => {
+  const { img, name, age, breed } = props.adoption.pet;
+  return (
+    <div>
+      <div className="adoption-item">
+        <div className="adoption-img-container">
+          <img className="adoption-img" src={img} />{" "}
+        </div>{" "}
+        <h3>
+          {" "}
+          <span> Pet Name ❣️ :  </span>
+          {name}
+        </h3>
+        <p>
+          {" "}
+          <span> Pet Age ❣️: </span>
+          {age}{" "}
+        </p>
+        <p>
+          {" "}
+          <span> Pet Breed ❣️: </span>
+          {breed}{" "}
+        </p>
+      </div>{" "}
+    </div>
+  );
+};
+
+export default AdoptionList;
