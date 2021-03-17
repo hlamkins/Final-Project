11fa13099d948367d53041f427be16cff451b247
Heather Lamkins <lamkinsheather@gmail.com>
2021-03-16 21:33:09 -0500

    updated nodemodules

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const bookRoutes = require("./pets");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/pets", bookRoutes);
 
 module.exports = router;
